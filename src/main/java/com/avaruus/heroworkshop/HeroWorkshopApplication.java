b8cc503170477107b8a479d339a649dbf58c9fa7
Justin Nichols <justin@nichols.mobi>
2019-08-13 16:34:13 -0400

    Added comments
    And updated gitignore to include likely paths

--- src/main/java/com/avaruus/heroworkshop/HeroWorkshopApplication.java
+++ src/main/java/com/avaruus/heroworkshop/HeroWorkshopApplication.java
@@ -16,15 +16,22 @@ public class HeroWorkshopApplication extends Application {
     private Parent rootNode;
 
     public static void main(final String[] args) {
+        // boostrap the JavaFX-enabled spring boot application
         launch(HeroWorkshopApplication.class, args);
     }
 
     @Override
     public void init() throws Exception {
+        // Initialize the spring boot application
         SpringApplicationBuilder builder = new SpringApplicationBuilder(HeroWorkshopApplication.class);
+
+        // build the spring context
         context = builder.run(getParameters().getRaw().toArray(new String[0]));
 
+        // load the main FXML file from the classpath, located in src/main/resources/fxml/main.fxml
         FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/main.fxml"));
+
+        // wire JavaFX to Spring
         loader.setControllerFactory(context::getBean);
         rootNode = loader.load();
     }
@@ -34,15 +41,25 @@ public class HeroWorkshopApplication extends Application {
         double width = 800.0;
         double height = 600.0;
 
+        // create scene and set it on the JavaFX primary stage
         primaryStage.setScene(new Scene(rootNode, width, height));
+
+        // set the application window title
         primaryStage.setTitle("Hero Workshop");
+
+        // set the application icon
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/icons/d20.png")));
+
+        // center the application
         primaryStage.centerOnScreen();
+
+        // render the application
         primaryStage.show();
     }
 
     @Override
     public void stop() throws Exception {
+        // stop the spring context
         context.close();
     }
 
