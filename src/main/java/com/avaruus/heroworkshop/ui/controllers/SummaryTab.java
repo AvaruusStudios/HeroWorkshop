4896ab987de1f900e089c3d7c41c086fd5e10629
Troy Pilewski <12568877+troypilewski@users.noreply.github.com>
2019-08-28 13:30:32 -0700

    Edited the fx:ids for the AbilitiesTable; Created the AbilityModel to store the data for the AbilitiesTable; Added the db (.JSON) file

--- src/main/java/com/avaruus/heroworkshop/ui/controllers/SummaryTab.java
+++ src/main/java/com/avaruus/heroworkshop/ui/controllers/SummaryTab.java
@@ -1,9 +1,12 @@
 package com.avaruus.heroworkshop.ui.controllers;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.control.cell.*;
 import org.springframework.stereotype.Controller;
+import com.avaruus.hero.AbilityModel;
 
 /*
     SummaryTab is the code-behind file for src/main/resources/fxml/tabs/summary.fxml
@@ -11,9 +14,16 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class SummaryTab {
     // autowire a reference to the label in the Summary tab by its fx:id
-    @FXML private Label summaryLabel;
-    @FXML private ChoiceBox genderChoiceBox;
-    @FXML private ChoiceBox dominantHandChoiceBox;
+//    @FXML private Label summaryLabel;
+    @FXML private ComboBox cmbGender;
+    @FXML private ComboBox cmbDominantHand;
+    @FXML private TableView<AbilityModel> tblAbilities;
+    @FXML private TableColumn<AbilityModel, String> colAbilityName;
+    @FXML private TableColumn<AbilityModel, Integer> colAbilityScore;
+    @FXML private TableColumn<AbilityModel, Integer> colAbilityModifier;
+    @FXML private TableColumn<AbilityModel, Integer> colAbilityAdjustableScore;
+    @FXML private TableColumn<AbilityModel, Integer> colAbilitySpeciesAdjustment;
+    @FXML private TableColumn<AbilityModel, Integer> colAbilityMiscAdjustment;
 
     public SummaryTab() {
     }
@@ -23,14 +33,34 @@ public class SummaryTab {
         // set the text on the summary label
         // summaryLabel.setText("Summary");
         
-        // set the choice for the gender choicebox
-        genderChoiceBox.setItems(FXCollections.observableArrayList(
+        // sets the options for the gender ComboBox
+        cmbGender.setItems(FXCollections.observableArrayList(
             "Male", "Female")
         );
         
-        // set the choices for the handed choicebox
-        dominantHandChoiceBox.setItems(FXCollections.observableArrayList(
+        // sets the options for the handed ComboBox
+        cmbDominantHand.setItems(FXCollections.observableArrayList(
             "Right", "Left")
         );
+        
+        // displays the abilites data
+        colAbilityName.setCellValueFactory(new PropertyValueFactory<>("AbilityName"));
+        colAbilityScore.setCellValueFactory(new PropertyValueFactory<>("AbilityScore"));
+        colAbilityModifier.setCellValueFactory(new PropertyValueFactory<>("AbilityModifier"));
+        colAbilityAdjustableScore.setCellValueFactory(new PropertyValueFactory<>("AbilityAdjustableScore"));
+        colAbilitySpeciesAdjustment.setCellValueFactory(new PropertyValueFactory<>("AbilitySpeciesAdjustment"));
+        colAbilityMiscAdjustment.setCellValueFactory(new PropertyValueFactory<>("AbilityMiscAdjustment"));
+        
+        // add data to table
+        tblAbilities.setItems(AbilitiesModel);
     }
+    
+    private ObservableList<AbilityModel> AbilitiesModel = FXCollections.observableArrayList(
+        new AbilityModel("Strength", 0, 0, 0, 0, 0),
+        new AbilityModel("Dexterity", 0, 0, 0, 0, 0),
+        new AbilityModel("Constitution", 0, 0, 0, 0, 0),
+        new AbilityModel("Intelligence", 0, 0, 0, 0, 0),
+        new AbilityModel("Wisdom", 0, 0, 0, 0, 0),
+        new AbilityModel("Charisma", 0, 0, 0, 0, 0)
+    );
 }
