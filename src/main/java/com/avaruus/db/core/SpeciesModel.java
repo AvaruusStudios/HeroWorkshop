cea4eb33d1d89fa42918853fe485699f44bd4fc5
Troy Pilewski <12568877+troypilewski@users.noreply.github.com>
2019-08-30 11:07:14 -0700

    added the Jackson ObjectMapper; create a SpeciesModel class; updated the core_rulebook.json

---
+++ src/main/java/com/avaruus/db/core/SpeciesModel.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.avaruus.db.core;
+
+import java.util.*;
+
+/**
+ * @name SpeciesModel.java
+ * @author Avaruus Studios
+ * @date Aug 29, 2019
+ */
+public class SpeciesModel {
+    private String name;
+    private String description;
+    private String personality;
+    private String appearance;
+    private String homeworld;
+    private String language;
+    private String background;
+    private List<String> knownlanguages;
+    private String size;
+    private String speed;
+    
+    public String getName() {
+        return name;
+    }
+    
+    public String getDescription() {
+        return description;
+    }
+    
+    public String getPersonality() {
+        return personality;
+    }
+    
+    public String getAppearance() {
+        return appearance;
+    }
+    
+    public String getHomeworld() {
+        return homeworld;
+    }
+    
+    public String getLanguage() {
+        return language;
+    }
+    
+    public String getBackground() {
+        return background;
+    }
+    
+    public List<String> getKnownLanguages() {
+        return knownlanguages;
+    }
+    
+    public String getSize() {
+        return size;
+    }
+    
+    public String getSpeed() {
+        return speed;
+    }
+}
